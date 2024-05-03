78e76757a86ecacdfbc3b34579747522cbf2dc3b
marsaam <marsaamalia03@gmail.com>
2024-05-03 14:12:10 +0700

    crud surat masuk, view surat masuk done

--- app/Livewire/Surat/SuratKeluar.php
+++ app/Livewire/Surat/SuratKeluar.php
@@ -2,12 +2,129 @@
 
 namespace App\Livewire\Surat;
 
+use App\Models\SuratKeluar as ModelsSuratKeluar;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class SuratKeluar extends Component
 {
+    use WithFileUploads;
+
+    public $nama, $suratmasuk, $suratmasukId = null, $edit = false;
+    public $form = [
+        'jenis_agenda_tp' => null,
+        'kode_lama' => null,
+        'kode_baru' => null,
+        'nomor_surat' => null,
+        'opd_id' => null,
+        'tgl_surat' => null,
+        'tgl_terima' => null,
+        'acara' => null,
+        'tanggalBerangkat' => null,
+        'tanggalPulang' => null,
+        'jamMulai' => null,
+        'tempat' => null,
+        'perihal' => null,
+        // 'no_surat' => null,
+        'dok_surat' => null,
+    ];
+
+
+    public function mount($id = null)
+    {
+        $this->suratmasukId = $id;
+        if ($id) {
+            $this->getEdit($id);
+        } else {
+            $this->edit = false;
+        }
+    }
+
+    public function getEdit($id)
+    {
+        $this->edit = true;
+        $this->suratmasuk = ModelsSuratKeluar::findOrFail($id);
+        $this->form = array_intersect_key($this->suratmasuk->toArray(), $this->form);
+    }
+
+    public function save()
+    {
+        if ($this->edit === true) {
+            $this->storeUpdate();
+        } else {
+            $this->store();
+        }
+    }
+
+    public function store()
+    {
+        // Pastikan ada file yang diunggah sebelum menyimpan
+        if ($this->form['dok_surat']) {
+            // Mengunggah file dan mendapatkan path file
+            $path = $this->form['dok_surat']->store('dokumen', 'public');
+
+            // Menyimpan path file ke dalam database
+            ModelsSuratKeluar::create([
+                'jenis_agenda_tp' => $this->form['jenis_agenda_tp'],
+                'kode_lama' => $this->form['kode_lama'],
+                'kode_baru' => $this->form['kode_baru'],
+                'nomor_surat' => $this->form['nomor_surat'],
+                'opd_id' => $this->form['opd_id'],
+                'tgl_surat' => $this->form['tgl_surat'],
+                'tgl_terima' => $this->form['tgl_terima'],
+                'acara' => $this->form['acara'],
+                'tanggalBerangkat' => $this->form['tanggalBerangkat'],
+                'tanggalPulang' => $this->form['tanggalPulang'],
+                'jamMulai' => $this->form['jamMulai'],
+                'tempat' => $this->form['tempat'],
+                'perihal' => $this->form['perihal'],
+                'dok_surat' => $path
+            ]);
+        }
+
+        return redirect()-> to('/suratmasuk-index');
+    }
+
+
+    public function storeUpdate()
+{
+    $suratmasuk = ModelsSuratKeluar::findOrFail($this->suratmasukId);
+
+    // Jika ada file yang diunggah, perbarui kolom dok_surat
+    // if ($this->form['dok_surat']) {
+    //     // Memperbarui kolom dok_surat dengan path file yang baru
+    //     $path = $this->form['dok_surat']->store('dokumen', 'public');
+    // }
+
+    // Update kolom lainnya
+    $suratmasuk->update([
+        'jenis_agenda_tp' => $this->form['jenis_agenda_tp'],
+        'kode_lama' => $this->form['kode_lama'],
+        'kode_baru' => $this->form['kode_baru'],
+        'nomor_surat' => $this->form['nomor_surat'],
+        'opd_id' => $this->form['opd_id'],
+        'tgl_surat' => $this->form['tgl_surat'],
+        'tgl_terima' => $this->form['tgl_terima'],
+        'acara' => $this->form['acara'],
+        'tanggalBerangkat' => $this->form['tanggalBerangkat'],
+        'tanggalPulang' => $this->form['tanggalPulang'],
+        'jamMulai' => $this->form['jamMulai'],
+        'tempat' => $this->form['tempat'],
+        'perihal' => $this->form['perihal'],
+        // Perbarui kolom dok_surat hanya jika ada file yang diunggah
+        // 'dok_surat' => isset($path) ? $path : $suratmasuk->dok_surat
+    ]);
+
+    // Reset variabel setelah disimpan
+    $this->reset();
+
+    // Redirect ke halaman suratmasuk-index setelah data disimpan
+    return redirect()->to('/suratmasuk-index');
+}
+
     public function render()
     {
+        $surat = ModelsSuratKeluar::first(); // Contoh pengambilan data surat, sesuaikan dengan kebutuhan Anda
         return view('livewire.surat.surat-keluar');
     }
 }
--- resources/views/livewire/surat/surat-masuk.blade.php
+++ resources/views/livewire/surat/surat-masuk.blade.php
@@ -14,8 +14,9 @@
                             <div class="form-group row">
                                 <label class="col-lg-3 col-form-label">Pilih Jenis Surat</label>
                                 <div class="col-lg-9">
-                                    <select id="selectSurat" class="form-control" name="jenis_agenda_tp" wire:model='form.jenis_agenda_tp'>
-                                        <option value="" selected>Pilih</option>
+                                    <select id="selectSurat" class="form-control" name="jenis_agenda_tp"
+                                        wire:model='form.jenis_agenda_tp'>
+                                        <option value="none" selected>Pilih</option>
                                         <option value="JENIS_SURAT_TP_01">Surat Masuk (Agenda)</option>
                                         <option value="JENIS_SURAT_TP_02">Surat Masuk (Biasa)</option>
                                     </select>
@@ -30,35 +31,39 @@
                                 <label class="col-lg-3 col-form-label">Unggah Surat Masuk</label>
                                 <div class="col-lg-9">
                                     <input type="file" id="fileInput" class="form-control"
-                                        wire:model='form.dok_surat'>
+                                        wire:model='form.dok_surat' name="dok_surat">
                                 </div>
                             </div>
                         </div>
                         <div class="col-6">
-                            {{-- <div id="previewContainer"></div> --}}
+                                <embed id="preview" src="" type="application/pdf" width="100%"
+                                    height="600">
                         </div>
                         {{-- kode lama --}}
                         <div class="col-6">
-                            {{-- <div id="formTetap" style="display: none;"> --}}
+                            <div class="form0">
                                 <!-- Kode lama -->
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Kode (Lama)</label>
                                     <div class="col-lg-9">
-                                        <input type="text" class="form-control" name="kode_lama" wire:model='form.kode_lama'>
+                                        <input type="text" class="form-control" name="kode_lama"
+                                            wire:model='form.kode_lama'>
                                     </div>
                                 </div>
                                 <!--kode baru -->
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Kode (Baru)</label>
                                     <div class="col-lg-9">
-                                        <input type="text" class="form-control" name="kode_baru" wire:model='form.kode_baru'>
+                                        <input type="text" class="form-control" name="kode_baru"
+                                            wire:model='form.kode_baru'>
                                     </div>
                                 </div>
                                 <!--nomor surat -->
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Nomor Surat</label>
                                     <div class="col-lg-9">
-                                        <input type="text" class="form-control" name="nomor_surat" wire:model='form.nomor_surat'>
+                                        <input type="text" class="form-control" name="nomor_surat"
+                                            wire:model='form.nomor_surat'>
                                     </div>
                                 </div>
                                 {{-- Pengirim --}}
@@ -76,65 +81,77 @@
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Tanggal Surat</label>
                                     <div class="col-lg-9">
-                                        <input type="date" class="form-control" name="tgl_surat" wire:model='form.tgl_surat'>
+                                        <input type="date" class="form-control" name="tgl_surat"
+                                            wire:model='form.tgl_surat'>
                                     </div>
                                 </div>
                                 {{-- tgl terima --}}
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Tanggal Terima</label>
                                     <div class="col-lg-9">
-                                        <input type="date" class="form-control" name="tgl_terima" wire:model='form.tgl_terima'>
+                                        <input type="date" class="form-control" name="tgl_terima"
+                                            wire:model='form.tgl_terima'>
                                     </div>
                                 </div>
-                            {{-- </div> --}}
-                            {{-- <div id="formJENIS_SURAT_TP_01" style="display: none;"> --}}
+                            </div>
+
+                            <div class="form1">
                                 {{-- subject --}}
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Subject</label>
                                     <div class="col-lg-9">
-                                        <input type="text" class="form-control" name="acara" wire:model='form.acara'>
+                                        <input type="text" class="form-control" name="acara"
+                                            wire:model='form.acara'>
                                     </div>
                                 </div>
                                 {{-- tgl mulai --}}
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Tanggal Mulai</label>
                                     <div class="col-lg-9">
-                                        <input type="date" class="form-control" name="tanggalBerangkat" wire:model='form.tanggalBerangkat'>
+                                        <input type="date" class="form-control" name="tanggalBerangkat"
+                                            wire:model='form.tanggalBerangkat'>
                                     </div>
                                 </div>
                                 {{-- tgl selesai --}}
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Tanggal Selesai</label>
                                     <div class="col-lg-9">
-                                        <input type="date" class="form-control" name="tanggalPulang" wire:model='form.tanggalPulang'>
+                                        <input type="date" class="form-control" name="tanggalPulang"
+                                            wire:model='form.tanggalPulang'>
                                     </div>
                                 </div>
                                 {{-- jam mulai --}}
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Jam Mulai</label>
                                     <div class="col-lg-9">
-                                        <input type="time" class="form-control" name="jamMulai" wire:model='form.jamMulai'>
+                                        <input type="time" class="form-control" name="jamMulai"
+                                            wire:model='form.jamMulai'>
                                     </div>
                                 </div>
                                 {{-- tempat --}}
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">Tempat</label>
                                     <div class="col-lg-9">
-                                        <input type="text" class="form-control" name="tempat" wire:model='form.tempat'>
+                                        <input type="text" class="form-control" name="tempat"
+                                            wire:model='form.tempat'>
                                     </div>
                                 </div>
-                            {{-- </div> --}}
-                            {{-- <div id="formJENIS_SURAT_TP_02" style="display: none;"> --}}
+                            </div>
+
+                            <div class="form2">
                                 {{-- perihal --}}
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label">perihal</label>
                                     <div class="col-lg-9">
-                                        <input type="text" class="form-control" name="perihal" wire:model='form.perihal'>
+                                        <input type="text" class="form-control" name="perihal"
+                                            wire:model='form.perihal'>
                                     </div>
                                 </div>
-                            {{-- </div> --}}
+                            </div>
+
                             <div class="text-right">
-                                <button type="submit" class="btn btn-primary">{{ $edit ? 'Simpan Perubahan' : 'Buat SUrat Masuk Baru' }}<i
+                                <button type="submit"
+                                    class="btn btn-primary">{{ $edit ? 'Simpan Perubahan' : 'Buat SUrat Masuk Baru' }}<i
                                         class="icon-paperplane ml-2"></i></button>
                             </div>
                 </form>
@@ -143,63 +160,57 @@
     </div>
 </div>
 
-{{-- pilih jenis surat --}}
-{{-- <script>
-    // Mendapatkan elemen select dan semua elemen form input
-    const selectSurat = document.getElementById("selectSurat");
-    const formTetap = document.getElementById("formTetap");
-    const formJENIS_SURAT_TP_01 = document.getElementById("formJENIS_SURAT_TP_01");
-    const formJENIS_SURAT_TP_02 = document.getElementById("formJENIS_SURAT_TP_02");
 
-    // Fungsi untuk menampilkan atau menyembunyikan form input berdasarkan pilihan
-    function toggleForms() {
-        // Semua form disembunyikan terlebih dahulu
-        formTetap.style.display = "none";
-        formJENIS_SURAT_TP_01.style.display = "none";
-        formJENIS_SURAT_TP_02.style.display = "none";
 
-        // Menampilkan form yang sesuai dengan pilihan pada select option
-        if (selectSurat.value === "JENIS_SURAT_TP_01") {
-            formTetap.style.display = "block";
-            formJENIS_SURAT_TP_01.style.display = "block";
-        } else if (selectSurat.value === "JENIS_SURAT_TP_02") {
-            formTetap.style.display = "block";
-            formJENIS_SURAT_TP_02.style.display = "block";
-        }
-    }
-
-    // Panggil fungsi toggleForms saat nilai select berubah
-    selectSurat.addEventListener("change", toggleForms);
-
-    // Panggil fungsi toggleForms saat halaman dimuat untuk menampilkan form sesuai dengan nilai awal select
-    window.onload = toggleForms;
-</script> --}}
-
-{{-- preview dokumen --}}
 <script>
-    const fileInput = document.getElementById("fileInput");
-    const previewContainer = document.getElementById("previewContainer");
+    $(document).ready(function() {
+        $('select[name=jenis_agenda_tp]').change(function() {
+            let isi = $(this).val();
 
-    fileInput.addEventListener("change", function() {
-        const file = this.files[0];
+            if (isi == 'none') {
+                $('.form0').hide('slow');
+                $('.form1').hide('slow');
+                $('.form2').hide('slow');
+            }
+            
+            if (isi == 'JENIS_SURAT_TP_01') {
+                $('.form0').show();
+                $('.form1').show('slow');
+            } else {
+                $('.form1').hide('slow');
+            }
 
-        if (file) {
-            const reader = new FileReader();
+            if (isi == 'JENIS_SURAT_TP_02') {
+                $('.form0').show();
+                $('.form2').show('slow');
+            } else {
+                $('.form2').hide('slow');
+            }
 
-            reader.onload = function(event) {
-                const pdfUrl = event.target.result;
-                const iframe = document.createElement("iframe");
-                iframe.src = pdfUrl;
-                iframe.width = "600";
-                iframe.height = "850";
-                previewContainer.innerHTML = ""; // Clear previous preview
-                previewContainer.appendChild(iframe);
+        });
+    });
+</script>
+
+<script>
+    $(document).ready(function() {
+        $('input[name=dok_surat]').change(function() {
+            let dokumen = $(this).prop('files')[0];
+
+            // Membuat objek FileReader
+            let reader = new FileReader();
+
+            // Event handler untuk menangani saat file selesai dibaca
+            reader.onload = function(e) {
+                // Mengambil URL hasil pembacaan file sebagai sumber gambar
+                let previewUrl = e.target.result;
+
+                // Menampilkan preview file
+                $('#preview').attr('src', previewUrl);
             };
 
-            reader.readAsDataURL(file);
-        } else {
-            previewContainer.innerHTML = ""; // Clear preview if no file selected
-        }
+            // Membaca konten file sebagai URL data
+            reader.readAsDataURL(dokumen);
+        });
     });
 </script>
 

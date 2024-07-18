5b1a841b9af99e2f1fe7a570885a780147f5ea7a
vandewa <devandewananta@gmail.com>
2024-07-18 14:51:13 +0700

    smeentara

--- app/Livewire/Pegawai/DataPegawai.php
+++ app/Livewire/Pegawai/DataPegawai.php
@@ -38,22 +38,24 @@ class DataPegawai extends Component
         //     ->where('idjenkedudupeg', 1)
         //     ->get();
 
-        $this->tb01 = Tb01::with(['skpd'])->select('tmlhr','photo','tb_01.tglhr','nip','tb_01.kdunit','email','gdp','gdb','email_dinas','nama','tb_01.idskpd',"jabatan.skpd", 'a_golruang.idgolru',  DB::Raw("
+        $this->tb01 = Tb01::with(['skpd'])->select('tmlhr', 'photo', 'tb_01.tglhr', 'nip', 'tb_01.kdunit', 'email', 'gdp', 'gdb', 'email_dinas', 'nama', 'tb_01.idskpd', "jabatan.skpd", 'a_golruang.idgolru', DB::Raw("
          case when jabfung is null and jabfungum is null then jabatan.jab
             when jabfung is null then jabfungum
             else  jabfung end as jabatan
-        "),DB::Raw("a_jenjurusan.jenjurusan as pendidikan"),DB::Raw("a_golruang.pangkat as pangkat"),DB::Raw("a_golruang.golru as golru"),DB::Raw("induk.skpd as unor"))
-         ->leftJoin('a_skpd as jabatan', "tb_01.idskpd", "jabatan.idskpd")
-         ->leftJoin('a_jenjurusan', "tb_01.idjenjurusan", "a_jenjurusan.idjenjurusan")
-         ->leftJoin('a_skpd as induk', DB::Raw("substring(tb_01.idskpd,1,2)"),'=',"induk.idskpd")
-         ->leftJoin('a_golruang', "tb_01.idgolrupkt", "a_golruang.idgolru")
-         ->leftJoin('a_jabfungum', "tb_01.idjabfungum", "a_jabfungum.idjabfungum")
-         ->leftJoin('a_jabfung', "tb_01.idjabfung", "a_jabfung.idjabfung")
-         ->where('tb_01.kdunit', $kdunit)
-         ->where('idjenkedudupeg', 1)
-         ->get();
+        "), DB::Raw("a_jenjurusan.jenjurusan as pendidikan"), DB::Raw("a_golruang.pangkat as pangkat"), DB::Raw("a_golruang.golru as golru"), DB::Raw("induk.skpd as unor"))
+            ->leftJoin('a_skpd as jabatan', "tb_01.idskpd", "jabatan.idskpd")
+            ->leftJoin('a_jenjurusan', "tb_01.idjenjurusan", "a_jenjurusan.idjenjurusan")
+            ->leftJoin('a_skpd as induk', DB::Raw("substring(tb_01.idskpd,1,2)"), '=', "induk.idskpd")
+            ->leftJoin('a_golruang', "tb_01.idgolrupkt", "a_golruang.idgolru")
+            ->leftJoin('a_jabfungum', "tb_01.idjabfungum", "a_jabfungum.idjabfungum")
+            ->leftJoin('a_jabfung', "tb_01.idjabfung", "a_jabfung.idjabfung")
+            ->where('tb_01.idskpd', 'like', $kdunit . '%')
+            ->where('idjenkedudupeg', 1)
+            ->orderBy('tb_01.idstspeg', 'asc')
+            ->orderBy('tb_01.idgolrupkt', 'desc')
+            ->get();
 
-        //  dd($this->tb01);
+        // dd($this->tb01); 
     }
     public function render()
     {
